b58e34232c059a4bd6f89f07518d01296882f005
Nikhil Maheshwari <79540086+nikhilbor@users.noreply.github.com>
2021-04-18 20:18:52 +0800

    Create audio.h

---
+++ audio.h
@@ -0,0 +1,35 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+#include "Subject.hpp"
+#include "Observer.hpp"
+
+/**
+ * A concrete implementation of the Subject interface
+ */
+class AudioData : public Subject {
+    std::vector<Observer *> observers; // observers
+
+    std::string Song = "Default Song";
+    std::string Artist = "Default Artist";
+    std::string Album = "Default Album";
+    int class_type = 2;
+
+public:
+
+    void registerObserver(Observer *observer) override;
+
+    void removeObserver(Observer *observer) override;
+
+    void notifyObservers() override;
+
+    /**
+     * Set the new state of the Audio data
+     * @param Song
+     * @param Artist
+     * @param Album
+     */
+    void setState(std::string Song, std::string Artist, std::string Album);
+
+};
