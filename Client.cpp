47b956e02c2dec1e05fc218cb49702bfe826fc1e
Nikhil Maheshwari <79540086+nikhilbor@users.noreply.github.com>
2021-04-18 20:15:04 +0800

    Create Client.cpp

---
+++ Client.cpp
@@ -0,0 +1,31 @@
+#include "Client.hpp"
+
+void Client::update(int type) {
+    // print the changed values
+    total_count ++;
+    switch(type)
+    {
+    case 1:
+            weather_count ++;
+    break;
+    case 2:
+            audio_count ++;
+    break;
+    case 3:
+            navi_count ++;
+    break;
+    default:
+        break;
+
+    }
+    std::cout << "Total Messages received " <<total_count << std::endl;
+    std::cout << "weather Messages  is " << weather_count << ", audio Messages  is " << audio_count << ", navi Messages  is " << navi_count<< std::endl;
+}
+int Client::total_count = 0;
+int Client::weather_count = 0;
+int Client::audio_count = 0;
+int Client::navi_count = 0;
+
+Client::Client(int id) {
+    this->id = id;
+}
